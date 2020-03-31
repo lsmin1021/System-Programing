5afe53f6c7ccd7ddf89b829127773eaae8fd9c67
lsmin1021 <lsmin@cspc.me>
2020-03-31 22:44:30 +0900

    history 기능 추가

--- sic.c
+++ sic.c
@@ -15,7 +15,7 @@ typedef struct HISTORY_{
 	struct HISTORY_* link;
 }HISTORY;
 
-HISTORY* h_head = null;
+HISTORY* h_head = NULL;
 int history_num = 0; //현재 히스토리에 저장된 개수
 
 void help(){ //명령어 목록 출력
@@ -25,20 +25,40 @@ void help(){ //명령어 목록 출력
 	
 }
 
-int history(){ //입력한 명령어 기록 
-	if(h_head == null){
-		
+int history(char input[]){ //입력한 명령어 기록 
+	HISTORY* new_node;
+
+	if(h_head == NULL){
+		new_node = (HISTORY*)malloc(sizeof(HISTORY)); //첫 기록 초기화
+		new_node->num = ++history_num;
+		strcpy(new_node->command_name, input);
+		new_node->link = NULL;
+		h_head = new_node;
+	}
+	else{
+		HISTORY* temp_node = h_head;
+		while(temp_node->link != NULL){
+			temp_node = temp_node->link;
+		}
+		new_node = (HISTORY*)malloc(sizeof(HISTORY)); //새 기록 초기화
+		new_node->num = ++history_num;
+		strcpy(new_node->command_name, input);
+		new_node->link = NULL;
+		temp_node->link = new_node;
 	}
 
-
-
+	return 1;
 }
 
 void print_history(char input[]){ //명령어 기록 출력
-	
+	HISTORY* temp_node = h_head;
+	history(input); //history도 명령어 내역에 추가
+	while(temp_node!=NULL){
+		printf("%-5d %s\n", temp_node->num, temp_node->command_name); //명령어 내역 출력
+		temp_node = temp_node->link;
+	}
 
-
-
+	return;
 }
 
 int main(void){
@@ -52,14 +72,15 @@ int main(void){
 
 		if(strcmp(input,"q") * strcmp(input,"quit") == 0) break; //quit 혹은 q 입력 시 종료
 		
-		history(input);
-
+		//history 혹은 h 입력 시 히스토리 출력
+		if(strcmp(input,"h") * strcmp(input,"history")==0) print_history(input);
+		else history(input);
 //		printf("\n%s\n",input);
 
 	}
 
 
-	help();
+//	help();
 
 	return 0;
 }
